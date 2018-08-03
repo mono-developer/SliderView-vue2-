b88966ca79f42805c715408a54e08e6639ee2048
Developer <minorumadam@hotmail.com>
2018-08-03 11:40:16 +0100

    update server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
 
-// app.use(serveStatic(__dirname + "/dist"));
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-// console.log('server started ' + port);
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port ' + port)
 });
-
-app.listen(process.env.PORT || 5000);
