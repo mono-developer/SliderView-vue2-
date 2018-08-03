cb3a84422509ef7608bdaf2fe935d230440c1be6
Developer <minorumadam@hotmail.com>
2018-08-03 11:36:01 +0100

    add server

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started ' + port);
+
+// app.use(serveStatic(__dirname + "/dist"));
+// var port = process.env.PORT || 5000;
+// app.listen(port);
+// console.log('server started ' + port);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(process.env.PORT || 5000);
